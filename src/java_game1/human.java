9da2bf57d01d185022db9c65908d803059d1bfd2
Kim Jun Ha <junhaha3@gamil.com>
2021-07-23 19:34:07 +0900

    인물 클래스 수정 테스트

--- src/java_game1/human.java
+++ src/java_game1/human.java
@@ -4,9 +4,16 @@ public class human {
 	String name = "¾Æ¹«°³";
 	int age = 0;
 	int location = 1;
+	int STR = 0;
+	int INT = 0;
+	int ATT = 0;
 	
-	public human(String name, int age) {
+	
+	public human(String name, int age, int STR, int INT, int ATT) {
 		this.name = name;
 		this.age = age;
+		this.STR = STR;
+		this.INT = INT;
+		this.ATT = ATT;
 	}
 }
