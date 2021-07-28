f105545961af04c948047494a260ebf5c60f6070
Kim Jun Ha <junhaha3@gamil.com>
2021-07-28 20:12:49 +0900

    인물 능력치 수정

--- src/java_game1/human_list.java
+++ src/java_game1/human_list.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class human_list {
 	public ArrayList<human> Human = new ArrayList<human>();
 	public human_list() {
-		Human.add(new human("±èÁØÇÏ", 21, 80, 75, 85, 1));
-		Human.add(new human("ÀÌ¼ø½Å", 45, 90, 99, 99, 2));
-		Human.add(new human("±ÇÀ²", 36, 89, 70, 70, 1));
-		Human.add(new human("°ûÀç¿ì", 27, 99, 60, 50, 1));
-		Human.add(new human("¿ø±Õ", 50, 50, 45, 35, 2));
+		Human.add(new human("±¤°³Åä´ë¿Õ", 21, 100, 60, 85, 1));
+		Human.add(new human("±ÙÃÊ°í¿Õ", 21, 85, 90, 90, 2));
+		Human.add(new human("ÁøÈï¿Õ", 20, 70, 90, 100, 4));
+		Human.add(new human("±èÀ¯½Å", 19, 85, 90, 90, 4));
+		Human.add(new human("¿¬°³¼Ò¹®", 26, 90, 85, 85, 1));
 	}
 }
