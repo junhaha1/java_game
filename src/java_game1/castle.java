0a9fd9d6eb8c1362a999eede64c13786e006a072
Kim Jun Ha <junhaha3@gamil.com>
2021-07-28 20:11:28 +0900

    성 이름, x/y좌표 속성 추가

--- src/java_game1/castle.java
+++ src/java_game1/castle.java
@@ -1,6 +1,7 @@
 package java_game1;
 
 public class castle {
+	String name = "";
 	
 	int pop = 0; //인구
 	int gold = 0; //금
@@ -10,15 +11,23 @@ public class castle {
 	int land = 0; //토지
 	int com = 0; //상업
 	
+	int x_coo = 0; //성 x좌표
+	int y_coo = 0; // 성 y좌표
+	
 	int code = 999; // 성 코드
 	
-	public castle(int POP, int GOLD, int RICE, int SPEC, int LAND, int COM, int CODE) {
+	public castle(String NAME, int POP, int GOLD, int RICE, int SPEC, int LAND, int COM, int X_COO, int Y_COO, int CODE) {
+		name = NAME;
 		pop = POP;
 		gold = GOLD;
 		rice = RICE;
 		spec = SPEC;
 		land = LAND;
 		com = COM;
+		
+		x_coo = X_COO;
+		y_coo = Y_COO;
+		
 		code = CODE;
 	}
 }
